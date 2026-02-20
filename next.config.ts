5e16604994704b76418dafee79b3c688b118a463
Juan Pablo Brea <juanbrearuiz@gmail.com>
2026-02-20 14:11:34 -0300

    Fix McKinsey logo and increase logo sizes in PartnershipsSection

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'lovelytics.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.b20southafrica.org',
+      },
     ],
   },
 };
